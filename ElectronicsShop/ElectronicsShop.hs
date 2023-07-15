88cfc211dae5c1ff87bd271ed7cc713e0bf79a6e
Mohsin-Ul-Islam <mohsinulislam180@gmail.com>
2023-07-15 22:50:27 +0500

    refactor: prepend -1 to list & get rid of conditional guards

--- ElectronicsShop/ElectronicsShop.hs
+++ ElectronicsShop/ElectronicsShop.hs
@@ -3,12 +3,9 @@
 module Main where
 
 solve :: [Int] -> Int
-solve (b:n:m:rest)
-    | length ps > 0 = maximum ps
-    | otherwise = -1
+solve (b:n:_:rest) = maximum (-1 : [x + y | x <- ks, y <- us, x + y <= b])
     where ks = take n rest -- keyboards
           us = drop n rest -- usb drives
-          ps = [x + y | x <- ks, y <- us, x + y <= b] -- possible permutations of keyboards & usb drives
 
 main :: IO()
 main = interact $ show . solve . map read . words
